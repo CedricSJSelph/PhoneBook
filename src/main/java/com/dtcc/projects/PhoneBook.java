e35d1d7ee021b849a2ca30eb06d5b882c26d4fd7
koastiebratt@gmail.com <you@example.com>
2020-10-01 22:37:43 -0400

    What i've done so far

--- src/main/java/com/dtcc/projects/PhoneBook.java
+++ src/main/java/com/dtcc/projects/PhoneBook.java
@@ -1,4 +1,58 @@
 package com.dtcc.projects;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
-}
+
+        //Phone number no hyphens please
+        public static HashMap<String, String> PhoneBook = new HashMap<>();
+        private static String Name;
+        private static String PhoneNumber;
+
+        public void add(String name, String phoneNumber){
+            PhoneBook.put(name, phoneNumber);
+        }
+
+        public static void addAll(String name, String[] phoneNumbers){
+            for(String temp:phoneNumbers){
+                PhoneBook.put(name, temp);
+            }
+        }
+
+        public static void remove(String name){
+            PhoneBook.remove(name);
+        }
+
+        public static boolean hasEntry(String name){
+           return PhoneBook.containsKey(name);
+        }
+
+        public List<String> lookup(String name){
+            List<String> temp = new ArrayList<>();
+            for(String val: PhoneBook.keySet()){
+                if(val.equals(name)){
+                    temp.add(PhoneBook.get(val));
+                }
+            }
+            return temp;
+        }
+
+//        public String reverseLookup(String phoneNumber){
+//            return PhoneBook.keys(phoneNumber)
+//        }
+
+        public static String getAllContactNames(){
+            String temp = "";
+            //Entries, each entry
+            for(Map.Entry m: PhoneBook.entrySet()){
+                String key = (String) m.getKey();
+                String value = (String) m.getValue();
+                temp += (key + " -> " + value + "\n");
+            }
+            return temp;
+        }
+    }
+
--- src/test/java/com/dtcc/projects/PhoneBookTest.java
+++ src/test/java/com/dtcc/projects/PhoneBookTest.java
@@ -1,4 +1,53 @@
 package com.dtcc.projects;
 
-public class PhoneBookTest {
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class PhoneBookTest extends PhoneBook{
+
+    @Test
+    public void addTest(){
+        add("Cedric", "3023007497");
+        add("Emergency","911");
+
+        Assert.assertEquals("3023007497", PhoneBook.get("Cedric"));
+        Assert.assertEquals("911", PhoneBook.get("Emergency"));
+    }
+
+    @Test
+    public void addAllTest(){
+        String[] temp = {"911", "3023007497"};
+        addAll("Cedric", temp);
+        Assert.assertEquals("3023007497", PhoneBook.get("Cedric"));
+    }
+
+    @Test
+    public void removeTest(){
+        add("Cedric", "3023007497");
+        add("Emergency","911");
+        remove("Emergency");
+        Assert.assertEquals(false, PhoneBook.containsKey("Emergency"));
+        Assert.assertEquals(true, PhoneBook.containsKey("Cedric"));
+    }
+
+    @Test
+    public void hasEntryTest(){
+        add("Cedric", "3023007497");
+        Assert.assertEquals(true, hasEntry("Cedric"));
+    }
+
+    @Test
+    public void lookupTest(){
+        add("Cedric", "3023007497");
+        List<String> temp = lookup("Cedric");
+        Assert.assertEquals("3023007497", temp.get(0));
+    }
+
+    @Test
+    public void getAllContactNamesTest(){
+        
+    }
 }
